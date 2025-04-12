6bb936506add967c913d540c3498c93d660f28bd
ozgecanodemis <odemisozgecan@gmail.com>
2025-04-13 01:30:38 +0300

    T2 completed

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -6,37 +6,38 @@ import { Pagination } from 'swiper/modules';
 
 const Carousel = () => {
     return (
-        <div className="mx-auto w-[414px] sm:w-full lg:w-[1440px]">
+        <div className="mx-auto w-[414px] sm:w-full lg:w-auto m-8">
             <Swiper
                 pagination={{ clickable: true }}
                 modules={[Pagination]}
                 className="h-[200px] sm:h-[300px] md:h-[400px] lg:h-[500px]"
             >
                 <SwiperSlide>
-                    <button className="btn-primary mt-4">Start Now</button>
-                    <img
-                        src="/images/slide1.jpg"
-                        alt="Slide 1"
-                        className="w-full h-full object-cover"
-                    />
+                    <div
+                        className="w-full h-full bg-cover bg-center flex items-center justify-center flex-col p-4"
+                        style={{ backgroundImage: "url('https://picsum.photos/1440/300')" }}
+                    >
+                        <h2 className='text-textColorWhite font-bold'>GROCERIES DELIVERY</h2>
+                        <p className='text-textColorWhite '>We know how large objects will act, but things on a
+                            small scale just do not act that way.</p>
+                        <button className="btn-primary mt-4">Start Now</button>
+                    </div>
+                </SwiperSlide>
 
-                </SwiperSlide>
                 <SwiperSlide>
-                    <img
-                        src="/images/slide2.jpg"
-                        alt="Slide 2"
-                        className="w-full h-full object-cover"
-                    />
+                    <div
+                        className="w-full h-full bg-cover bg-center"
+                        style={{ backgroundImage: "url('https://picsum.photos/1440/303" }}
+                    ></div>
                 </SwiperSlide>
+
                 <SwiperSlide>
-                    <img
-                        src="/images/slide3.jpg"
-                        alt="Slide 3"
-                        className="w-full h-full object-cover"
-                    />
+                    <div
+                        className="w-full h-full bg-cover bg-center"
+                        style={{ backgroundImage: "url('https://picsum.photos/1440/305')" }}
+                    ></div>
                 </SwiperSlide>
             </Swiper>
-
         </div>
     );
 };
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,6 +4,7 @@ import MostPopular from "../components/MostPopular";
 import ProductSection from "../components/ProductSection";
 import CategoryPanel from "../components/CategoryPanel";
 import Blog from "../components/Blog";
+import Brands from "../components/brands";
 
 
 const HomePage = () => {
@@ -14,16 +15,14 @@ const HomePage = () => {
             <div className="flex flex-col lg:flex-row gap-4 p-4 max-w-screen-xl mx-auto">
                 <ProductSection />
                 <CategoryPanel />
-
             </div>
             <MostPopular />
-
             <div className="flex flex-col lg:flex-row gap-4 p-4 max-w-screen-xl mx-auto">
                 <ProductSection />
                 <CategoryPanel />
-
             </div>
             <MostPopular />
+            <Brands />
             <Blog />
         </>
     );
